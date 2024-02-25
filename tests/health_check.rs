4e6aedb4f9b2c64dd2192c6391a3fc937f26ce45
Rexfordasamoah51 <rexfordasamoah51@gmail.com>
2024-02-25 20:45:55 +0000

    chore: added subscribe implementation(test-case)

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,12 +18,58 @@ async fn health_check_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String{
+#[tokio::test]
+async fn subscribe_valid() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to run");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_invalid() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid, error) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid)
+            .send()
+            .await
+            .expect("Failed to send");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The Api failed with 400 and payload was {}",
+            error
+        );
+    }
+}
+
+fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind");
     let port = listener.local_addr().unwrap().port();
     let server = emailserver::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
-    
+
     format!("http://127.0.0.1:{}", port)
 }
